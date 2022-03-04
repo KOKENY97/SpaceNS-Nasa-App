ea8d36cf0cf77fd03a5f0b115169b3aec77f5bdb
Cahue Kokeny <cahue.kokeny@hotmail.com>
2022-03-04 15:08:15 -0500

    try POST request

--- Nasa Space App/Controller/Planets/PlanetsViewController.swift
+++ Nasa Space App/Controller/Planets/PlanetsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class PlanetsViewController: UIViewController {
     
@@ -20,9 +21,12 @@ class PlanetsViewController: UIViewController {
     @IBOutlet weak var queryPlanetButton: UIButton!
     @IBOutlet weak var menuButton: UIButton!
     
+    var habitable = String()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         registerButton.layer.borderWidth = 1
         registerButton.layer.borderColor = UIColor.white.cgColor
         registerButton.layer.cornerRadius = 9.0
@@ -40,9 +44,11 @@ class PlanetsViewController: UIViewController {
     @IBAction func habitableButtonPressed(_ sender: UIButton) {
         if habitableButton.isSelected == false {
             habitableButton.isSelected = true
+            habitable = "YES"
             habitableButton.backgroundColor = .blue
         } else {
             habitableButton.isSelected = false
+            habitable = "NO"
             habitableButton.backgroundColor = .white
         }
         
@@ -50,15 +56,50 @@ class PlanetsViewController: UIViewController {
     }
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
+        func postMethod() {
+            
+            let params: Parameters = [
+                "planetName": planetNameTxtField.text!,
+                "starName": starNameTxtField.text!,
+                "mass": massTxtField.text!,
+                "size": sizeTxtField.text!,
+                "habitable": habitable
+                
+            ]
+            
+            
+            Alamofire.request("https://desafionasa.herokuapp.com/planetas", method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).validate(statusCode: 200 ..< 299).responseJSON { AFdata in
+                do {
+                    guard let jsonObject = try JSONSerialization.jsonObject(with: AFdata.data!) as? [String: Any] else {
+                        print("Error: Cannot convert data to JSON object")
+                        return
+                    }
+                    guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
+                        print("Error: Cannot convert JSON object to Pretty JSON data")
+                        return
+                    }
+                    guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
+                        print("Error: Could print JSON in String")
+                        return
+                    }
+                    
+                    print(prettyPrintedJson)
+                } catch {
+                    print("Error: Trying to convert JSON data to string")
+                    return
+                }
+            }
+            
+        }
     }
     
     
     @IBAction func queryPlanetButtonPressed(_ sender: UIButton) {
     }
     
-   
+    
     @IBAction func menuButtonPressed(_ sender: UIButton) {
     }
     
-
+    
 }
