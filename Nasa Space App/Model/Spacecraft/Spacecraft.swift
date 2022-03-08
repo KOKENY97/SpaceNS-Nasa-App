6518bdc3f3a07229f74c2a6f1144b6d55e979881
Cahue Kokeny <cahue.kokeny@hotmail.com>
2022-03-07 19:52:25 -0500

    try post and get requests

---
+++ Nasa Space App/Model/Spacecraft/Spacecraft.swift
@@ -0,0 +1,8 @@
+//
+//  Spacecraft.swift
+//  Nasa Space App
+//
+//  Created by user209843 on 3/5/22.
+//
+
+import Foundation
---
+++ Nasa Space App/View/Spacecraft/SpacecraftCell/SpacecraftCell.swift
@@ -0,0 +1,12 @@
+//
+//  SpacecraftCell.swift
+//  Nasa Space App
+//
+//  Created by user209843 on 3/5/22.
+//
+
+import UIKit
+
+class SpacecraftCell: UICollectionViewCell {
+    
+}
